d769f689351049d139e21ba6b17af602dc3b484a
Kirill Leyfer <leyfer.kirill@gmail.com>
2025-12-24 19:55:36 +0300

    Add transition rules between screens

--- orbitris_esp32/game_main.cpp
+++ orbitris_esp32/game_main.cpp
@@ -1,5 +1,7 @@
 #include "game_main.h"
 
+#include <algorithm>
+
 #include "game_screen.h"
 #include "game_over_screen.h"
 #include "game_utils.h"
@@ -11,15 +13,39 @@
 Screen* current_screen = nullptr;
 Screen* new_screen = nullptr;
 
+struct TransitionRule {
+  Screen*& from;
+  Screen*& to;
+  TransitionKind kind;
+};
+
+TransitionRule rules[]{
+  { screens::game_screen, screens::game_over_screen, TransitionKind::ZOOM_OUT },
+  { screens::game_over_screen, screens::game_screen, TransitionKind::ZOOM_IN }
+};
+
 Stats stats{};
 
 static bool in_transition = false;
+
+static TransitionKind find_transition_rule(Screen* from, Screen* to) {
+  constexpr TransitionKind default_rule = TransitionKind::ZOOM_IN;
+
+  // Linear complexity, whatever
+  for (size_t i = 0; i < std::size(rules); i++) {
+    if (rules[i].from == from && rules[i].to == to) {
+      return rules[i].kind;
+    }
+  }
+
+  return default_rule;
+}
 
 void update_screen() {
   new_screen = current_screen->update();
   if (new_screen != current_screen) {
     in_transition = true;
-    start_transition(current_screen, new_screen, TransitionKind::ZOOM_IN);
+    start_transition(current_screen, new_screen, find_transition_rule(current_screen, new_screen));
   }
 }
 
@@ -29,8 +55,10 @@ void draw_screen() {
 
 void init_game() {
   init_trig_tables();
+
   screens::game_screen = new GameScreen(stats);
   screens::game_over_screen = new GameOverScreen(stats);
+
   current_screen = screens::game_screen;
   current_screen->init();
   in_transition = false;
